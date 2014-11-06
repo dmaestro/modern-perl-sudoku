4b9e3b111ef79c7fadcce8f16cc15ab6a0322638
Douglas L. Schrag <doug@theschrags.net>
2014-11-06 11:36:11 -0500

    cleanup for perlcritic

--- Sudoku/Cell.pm
+++ Sudoku/Cell.pm
@@ -8,20 +8,23 @@ use DLS::MooTypes;
 use namespace::clean;
 
 use DLS::Constants qw(:TINYTEXT);
+use Readonly;
+
+Readonly::Scalar  my $SUDOKU_DIMENSION  =>  9;
 
 has value => (
       is        =>  'rwp',
       isa       =>  sub {
                       DLS::MooTypes->Integer->($_[0]);
-                      confess "Out of range!" if $_[0] < 1 or $_[0] > 9;
+                      confess 'Out of range!' if $_[0] < 1 or $_[0] > $SUDOKU_DIMENSION;
                     },
       predicate =>  1,
 );
 
-sub set_value {
-  my $self = shift;
+sub set_value { ## no critic (FinalReturn)
+  my ($self, @args) = @_;
   confess 'Once set, value is immutable!' if $self->has_value;
-  $self->_set_value(@_);
+  $self->_set_value(@args);
 }
 
 has initial => (
@@ -29,7 +32,7 @@ has initial => (
       default   =>  $EMPTY_STRING,  # false
 );
 
-sub BUILD {
+sub BUILD { ## no critic (FinalReturn)
   my ($self) = @_;
   if ($self->has_value) {
     $self->_set_initial(1);
