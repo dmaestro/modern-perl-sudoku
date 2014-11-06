b321444f70f9b467f2478efaab70307c6c3486f7
Douglas L. Schrag <doug@theschrags.net>
2014-11-06 16:00:17 -0500

    Integer type includes negative numbers

--- DLS/MooTypes.pm
+++ DLS/MooTypes.pm
@@ -8,7 +8,7 @@ use Scalar::Util qw(looks_like_number);
 sub Integer {
   return sub {
     confess "Not an Integer!"
-      if not (looks_like_number($_[0]) and $_[0] =~ /^\d+\z/a);
+      if not (looks_like_number($_[0]) and $_[0] =~ /^-?\d+\z/a);
   }
 }
 
